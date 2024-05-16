ab88dd2e785a3a0cfa0c09beb0f843d48e4f0f49
disusen <gvidasrakauskas10@gmail.com>
2024-05-16 15:38:32 +0300

    Get match ids by summoner name and tagline
    Gets match ids by summer name and tagline

--- antife/Riot API/__main__.py
+++ antife/Riot API/__main__.py
@@ -1,4 +1,4 @@
-from helpers import get_summoner_info
+from helpers import get_summoner_info, get_match_ids
 
 summoner_name = "HKavav"
 tag_line = "EUN1"
@@ -6,3 +6,7 @@ tag_line = "EUN1"
 
 rank = get_summoner_info(summoner_name, tag_line)
 print(rank)
+
+match_ids = get_match_ids(summoner_name, tag_line)
+
+print(match_ids)
--- antife/Riot API/helpers.py
+++ antife/Riot API/helpers.py
@@ -49,20 +49,43 @@ def get_summoner_ids(puuid, region=settings.DEFAULT_REGION_CODE):
 
 def get_summoner_info(summoner_name, tag_line, region=settings.DEFAULT_REGION_CODE):
     """
-    Wrapper for SUMMONER-V4 API portal
-    Gets information about a summoner by their name
+    Wrapper for LEAGUE-V4 API portal
+    Gets information about a summoner by their name and tagline
     :return: Summoner information as a dictionary or None if there's an issue
     """
 
-    summoner = get_account_by_riot_id(summoner_name, tag_line)
-    id = get_summoner_ids(summoner['puuid'])
+    puuid = get_account_by_riot_id(summoner_name, tag_line)['puuid']
+    id = get_summoner_ids(puuid)['id']
 
 
     params = {
         'api_key': settings.API_KEY
     }
 
-    api_url = f"https://{region}.api.riotgames.com/lol/league/v4/entries/by-summoner/{id['id']}"
+    api_url = f"https://{region}.api.riotgames.com/lol/league/v4/entries/by-summoner/{id}"
+
+    try:
+        response = requests.get(api_url, params=urlencode(params))
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f'Issue getting summoner data from API: {e}')
+        return None
+
+def get_match_ids(summoner_name, tag_line, match_count=5, region=settings.DEFAULT_REGION):
+    """
+    Wrapper for MATCH-V5 API portal
+    Gets match ids depending on the count by summoner name and tag line
+    :return: Match ids as a dictionary or None if there's an issue
+    """
+
+    puuid = get_account_by_riot_id(summoner_name, tag_line)['puuid']
+    params = {
+        'count': match_count,
+        'api_key': settings.API_KEY
+    }
+
+    api_url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
 
     try:
         response = requests.get(api_url, params=urlencode(params))
