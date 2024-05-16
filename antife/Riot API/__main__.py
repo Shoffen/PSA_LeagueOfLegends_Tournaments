b7086b8f654eed63a27c6fe20308029729686498
disusen <gvidasrakauskas10@gmail.com>
2024-05-16 16:57:19 +0300

    Added example function on how to track player statistics in match

--- antife/Riot API/__main__.py
+++ antife/Riot API/__main__.py
@@ -16,14 +16,51 @@ print(summoner)
 
 # gets match ids, count default value 5
 match_ids = get_match_ids(summoner_name, tag_line, 2)
+print(match_ids[0])
 
-# Gets our players statistics in a match
-match = get_match(match_ids[0])
-if summoner_puuid in match['metadata']['participants']:     # Checking if our player was in this match
-    player_index = match['metadata']['participants'].index(summoner_puuid)      # Finding index of our player
-else:
-    None
-player_match_statistics = match['info']['participants'][player_index] # Grabs all the data associated with the player
-print(player_match_statistics)
-print(match['info']['participants'][player_index]['win']) # example usage to check if the player won
+
+def get_player_statistics_in_match():
+    # Gets our player's statistics in a match
+    match = get_match(match_ids[0])
+    if summoner_puuid in match['metadata']['participants']:  # Checking if our player was in this match
+        player_index = match['metadata']['participants'].index(summoner_puuid)  # Finding index of our player
+    else:
+        return None  # Return None if player not found in the match
+
+    player_match_statistics = match['info']['participants'][
+        player_index]  # Grabs all the data associated with the player
+
+    # Extracting required values
+    champion_name = player_match_statistics['championName']
+    champ_level = player_match_statistics['champLevel']
+    kills = player_match_statistics['kills']
+    deaths = player_match_statistics['deaths']
+    assists = player_match_statistics['assists']
+    lane = player_match_statistics['lane']
+    total_damage_dealt = player_match_statistics['totalDamageDealt']
+    total_damage_taken = player_match_statistics['totalDamageTaken']
+    total_minions_killed = player_match_statistics['totalMinionsKilled']
+    wards_killed = player_match_statistics['wardsKilled']
+    wards_placed = player_match_statistics['wardsPlaced']
+    win = player_match_statistics['win']
+
+    return {
+        'championName': champion_name,
+        'champLevel': champ_level,
+        'kills': kills,
+        'deaths': deaths,
+        'assists': assists,
+        'lane': lane,
+        'totalDamageDealt': total_damage_dealt,
+        'totalDamageTaken': total_damage_taken,
+        'totalMinionsKilled': total_minions_killed,
+        'wardsKilled': wards_killed,
+        'wardsPlaced': wards_placed,
+        'win': win
+    }
+
+
+print(get_player_statistics_in_match())
+
+
 
